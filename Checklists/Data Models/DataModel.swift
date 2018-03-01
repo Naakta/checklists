cfe2eb8facdda6e0befc1aa0c6f3ce17cd612d73
Douglas Wagner <doug.gwagner@gmail.com>
2018-03-01 11:18:58 -0500

    Added Features -First Launch handled - adds a checklist for the user named "List" and loads to that screen -File Management - data models, view controllers, and resources have been placed in groups

--- Checklists/Data Models/DataModel.swift
+++ Checklists/Data Models/DataModel.swift
@@ -11,15 +11,38 @@ import Foundation
 class DataModel {
     var lists = [Checklist]()
     
+    var indexOfSelectedChecklist: Int {
+        get {
+            return UserDefaults.standard.integer(forKey: "ChecklistIndex")
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: "ChecklistIndex")
+        }
+    }
+    
+    func registerDefaults() {
+        let dictionary: [String:Any] = ["ChecklistIndex": -1, "FirstTime": true]
+        UserDefaults.standard.register(defaults: dictionary) // These two could have been combined
+    }
     
     init() {
         loadChecklists()
         registerDefaults() // Needed to avoid crashing on empty "lists" array
+        handleFirstTime()
     }
     
-    func registerDefaults() {
-        let dictionary = ["ChecklistIndex": -1]
-        UserDefaults.standard.register(defaults: dictionary) // These two could have been combined
+    func handleFirstTime() {
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+        
+        if firstTime {
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            
+            indexOfSelectedChecklist = 0
+            userDefaults.set(false, forKey: "FirstTime")
+            userDefaults.synchronize()
+        }
     }
     
     func documentsDirectory() -> URL {
